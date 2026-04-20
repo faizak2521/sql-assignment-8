5a9cfa8bd510e7934ddf65e4377066795801a74a
Faiza Khan <161170273+faizak2521@users.noreply.github.com>
2026-04-20 18:18:04 -0500

    added model bare bones, needs server access.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace MyClass.Tables;
+
+public class Tables
+{
+    public int Id { get; set; }
+    public string ItemNum { get; set; } = "";
+    public string Description { get; set; } = "";
+    public int OnHand { get; set; }
+    public string Category { get; set; } = "";
+    public int Storehouse { get; set; }
+    public decimal Price { get; set; }
+}
+
+internal class ProductDbContext : DbContext
+{
+    public DbSet<Tables> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {   // Next Step: Needs docker access
+        optionsBuilder.UseSqlServer(@"Server=SERVER_NAME;Database=DATABASE_NAME;Trusted_Connection=True;TrustServerCertificate=True;");
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        using (ProductDbContext context = new ProductDbContext())
+        {
+        }
+    }
+}
